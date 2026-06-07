34db430e5293166219b8d2d87409d37fe2a36682
Gray <graygnass@gmail.com>
2026-06-07 19:35:19 -0400

    Fixed toggle plugin

--- init.lua
+++ init.lua
@@ -1,20 +1,7 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("config.visuals")
 require("config.diagnostics")
 require("config.lazy")
 
-require('lspconfig').clangd.setup({
-    cmd = {
-        "clangd",
-        "--background-index",
-        "--clang-tidy",
-        "--query-driver=/opt/homebrew/bin/g++*,/opt/homebrew/bin/gcc*"
-    },
-})
-
---Set mapleader to the Space bar before lazy loads
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
---Import the lazy.nvim configuration file
-require("config.lazy")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,26 +1,39 @@
 return {
   {
     "neovim/nvim-lspconfig",
-
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "hrsh7th/cmp-nvim-lsp",
     },
-
     config = function()
       require("mason").setup()
 
       require("mason-lspconfig").setup({
         ensure_installed = {
+          "clangd",
+          "lua_ls",
           "ts_ls",
           "html",
           "cssls",
-          "lua_ls",
         },
       })
 
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--query-driver=/opt/homebrew/bin/g++*,/opt/homebrew/bin/gcc*",
+        },
+      })
+
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+      })
 
       vim.lsp.config("ts_ls", {
         capabilities = capabilities,
@@ -34,19 +47,13 @@ return {
         capabilities = capabilities,
       })
 
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-      })
-
       vim.lsp.enable({
+        "clangd",
+        "lua_ls",
         "ts_ls",
         "html",
         "cssls",
-        "lua_ls",
       })
     end,
   },
 }
-
-
-
