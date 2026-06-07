82fa0e0f3746efa5bd10de1f3e4fa6683f70f89b
Gray <graygnass@gmail.com>
2026-06-07 19:23:36 -0400

    Added back key hints

--- init.lua
+++ init.lua
@@ -3,6 +3,16 @@ vim.g.maplocalleader = ' '
 require("config.visuals")
 require("config.diagnostics")
 require("config.lazy")
+
+require('lspconfig').clangd.setup({
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--query-driver=/opt/homebrew/bin/g++*,/opt/homebrew/bin/gcc*"
+    },
+})
+
 --Set mapleader to the Space bar before lazy loads
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
